b563a91ca218338f52277eb557a601f08251f741
clifforc <m.aleksandr@hotmail.com>
2025-09-06 12:41:22 +0300

    add genre model, add genre to admin panel

--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField("name", max_length=255)
+    description = models.TextField("description", blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'content"."genre'
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
