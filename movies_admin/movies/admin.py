1ed7bbcc26f0266ce0fe4f5277ca3c690a9a4239
clifforc <m.aleksandr@hotmail.com>
2025-09-06 16:22:50 +0300

    enhance admin panel: add list displays, search fields, and filters for Genre, Person, and FilmWork

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -12,14 +12,20 @@ class PersonFilmWorkInline(admin.TabularInline):
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "created", "modified")
+    search_fields = ("name",)
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("full_name", "created", "modified")
+    search_fields = ("full_name",)
 
 
 @admin.register(FilmWork)
 class FilmWorkAdmin(admin.ModelAdmin):
     inlines = (GenreFilmWorkInline, PersonFilmWorkInline)
+
+    list_display = ("title", "type", "creation_date", "rating", "created", "modified")
+    list_filter = ("type",)
+    search_fields = ("title", "description", "id")
