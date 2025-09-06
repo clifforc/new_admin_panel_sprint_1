442141dac00b679ecb3c97d660925667786e29c3
clifforc <m.aleksandr@hotmail.com>
2025-09-06 13:04:51 +0300

    add FilmWork model, add FilmWork to admin panel

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Genre
+from .models import FilmWork, Genre
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FilmWork)
+class FilmWorkAdmin(admin.ModelAdmin):
     pass
--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -9,7 +9,33 @@ class Genre(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
-        db_table = 'content"."genre'
+        db_table = "content\".\"genre"  # fmt: skip
         verbose_name = "Жанр"
         verbose_name_plural = "Жанры"
+
+
+class FilmWork(models.Model):
+    class FilmType(models.TextChoices):
+        MOVIE = "movie"
+        TV_SHOW = "tv_show"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField("title", blank=False, max_length=255)
+    description = models.TextField("description", blank=True)
+    creation_date = models.DateField("creation_date", blank=False)
+    rating = models.FloatField("rating", blank=True)
+    type = models.CharField("type", choices=FilmType.choices)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "content\".\"film_work"  # fmt: skip
+        verbose_name = "Кинопроизведение"
+        verbose_name_plural = "Кинопроизведения"
